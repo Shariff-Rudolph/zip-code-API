806a20a1b16345c1eb11056ccaa148cf5504fd76
Shariff Rudolph <shariff.rudolph@techs-MacBook-Pro-3.local>
2021-09-01 23:56:59 -0400

    create initial zip code test

---
+++ tests/zip-code-90210/zip-code.spec.js
@@ -0,0 +1,14 @@
+const schema = require('./schemas/united-states.js');
+
+describe('api.zippopotam.us/{country-abbreviation}/{zip-code}', function () {
+    it('Tests country abbreviation and zip code', function () {
+        cy.request({
+            url: 'api.zippopotam.us/us/90210',
+            method: 'GET',
+        }).then((response) => {
+            const { error } = schema.validate(response.body);
+            expect(error).to.be.undefined;
+            expect(response.body.country).to.equal('United States');
+        });
+    });
+});
