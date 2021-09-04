b0ccd15abfe6730ad83c8248a08b127ce0390065
Shariff Rudolph <shariff.rudolph@techs-MacBook-Pro-3.local>
2021-09-03 22:32:16 -0400

    add more assertions to cover API response

--- tests/zip-code-90210/zip-code.spec.js
+++ tests/zip-code-90210/zip-code.spec.js
@@ -6,9 +6,16 @@ describe('api.zippopotam.us/{country-abbreviation}/{zip-code}', function () {
             url: 'api.zippopotam.us/us/90210',
             method: 'GET',
         }).then((response) => {
+            const { items } = response.body;
             const { error } = schema.validate(response.body);
             expect(error).to.be.undefined;
+            expect(response.body).to.have.property('post code').to.contain(90210);
             expect(response.body.country).to.equal('United States');
+            expect(response.body).to.have.property('country abbreviation').to.equal('US');
+            expect(response.body.places[0].state).to.equal('California');
+            expect(response.body.places[0]).to.have.property('place name').to.equal('Beverly Hills');
+            expect(response.body.places[0].longitude).to.contain(-118.4065);
+            expect(response.body.places[0]).to.have.property('state abbreviation').to.equal('CA');
         });
     });
 });
